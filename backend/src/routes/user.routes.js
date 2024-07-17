6285a8929c434c597533fab9dc385b0d177a28e5
Abhishek <ay143488@gmail.com>
2024-07-17 22:47:53 +0530

    Added userRoutes for login/signup and utils

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { loginUser, logoutUser, registerUser, setTimeout, addTabData } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+
+const router=Router()
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/settimeout").post(verifyJWT,setTimeout);
+router.route("/addtabdata").post(verifyJWT,addTabData);
+export default router;
